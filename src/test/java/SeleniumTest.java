797b290223e105276d0d5978fe11c6ff742f2d2e
belaletech <belalahmad@lambdatest.com>
2024-08-29 17:17:33 +0530

    upload

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SeleniumTest {
+
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        // LambdaTest Hub URL
+        String hubURL = "https://belalahmad:cousQqH3syuMR3H55LiQfG4QqCyPHRsZs3XJ3mbEle94hOdYLj@hub.lambdatest.com/wd/hub";
+
+        // Desired Capabilities for LambdaTest
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
+        capabilities.setCapability(CapabilityType.BROWSER_VERSION, "latest");
+        capabilities.setCapability("platformName", "Windows 10"); // Change platform as needed
+        capabilities.setCapability("build", "Selenium Test Build");
+        capabilities.setCapability("name", "Selenium Test");
+        capabilities.setCapability("network", true); // Enable network logs
+        capabilities.setCapability("visual", true);  // Enable visual logs
+        capabilities.setCapability("video", true);   // Enable video recording
+        capabilities.setCapability("console", true); // Enable console logs
+
+        // Initiate the remote WebDriver with LambdaTest hub
+        driver = new RemoteWebDriver(new URL(hubURL), capabilities);
+
+        // Open the URL
+        driver.get("https://netflix-gpt-two-livid.vercel.app/");
+    }
+
+    @Test
+    public void testStep() {
+        // Locate elements and perform actions
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/form/div[1]/input")).sendKeys("ayan123@gmail.com");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/form/div[2]/input")).sendKeys("Ayan123@");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/form/div[2]/button")).click();
+
+        // Add more steps or assertions as needed
+    }
+}
